70d7bd34c23edad5e14ebf46ce0ba5a041ad955e
Ayshine <aysin.tasdelen@hotmail.com>
2018-07-04 18:03:42 +0300

    first C programming exercise : print a line

---
+++ c/Welcome_C.c
@@ -0,0 +1,13 @@
+/* A first program in C
+lines between slash + star and star + slash are called comments and don't cause the computer to perform any action */
+
+#include <stdio.h>
+
+/* function main begins program execution */
+int main(void)
+{
+	printf("Welcome to C!\n");
+
+	return 0; /*indicate that program ended successfully*/
+
+} /*end function main*/
